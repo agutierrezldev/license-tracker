97f74b48b899acbede61dd4898971245b2de280a
axelgutierrezlopezdev <axelgutierrezlopezdev@gmail.com>
2024-09-23 16:09:16 -0500

    add .env in project

--- config-server/src/main/java/com/agutierrezl/microservices/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/agutierrezl/microservices/config_server/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.agutierrezl.microservices.config_server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -9,6 +10,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+		// load to file .env
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("BRANCH", dotenv.get("BRANCH"));
+		// GITHUB
+		System.setProperty("GITHUB_USER", dotenv.get("GITHUB_USER"));
+		System.setProperty("GITHUB_TOKEN", dotenv.get("GITHUB_TOKEN"));
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
