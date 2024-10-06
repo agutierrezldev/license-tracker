13501aa760207d94d96380972a382f20f9e74b22
axelgutierrezlopezdev <axelgutierrezlopezdev@gmail.com>
2024-10-06 08:15:32 -0500

    add documentation to document-service with swagger

--- document-service/src/main/java/com/agutierrezl/document_service/controller/DocumentController.java
+++ document-service/src/main/java/com/agutierrezl/document_service/controller/DocumentController.java
@@ -75,25 +75,106 @@ public class DocumentController {
         return ResponseEntity.ok(documentService.getAll());
     }
 
-    @Operation(tags = "DocumentCommand")
+    @Operation(tags = "DocumentCommand", description = "Save a new document",
+            responses = {
+                    @ApiResponse(description = "Document created", responseCode = "201",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = DocumentDTO.class))),
+                    @ApiResponse(description = "Invalid request data", responseCode = "400",
+                            content = @Content(mediaType = "application/json"))
+            }
+    )
     @PostMapping("")
     public ResponseEntity<DocumentDTO> save(@Valid @RequestBody DocumentDTO documentDTO) {
         return ResponseEntity.ok(documentService.save(documentDTO));
     }
 
-    @Operation(tags = "DocumentCommand")
+    @Operation(
+            summary = "Update an existing document by ID",
+            description = "Updates the data of a specific document using its ID",
+            tags = {"DocumentCommand"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Document successfully updated",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = DocumentDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request, invalid data",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Document not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<DocumentDTO> update(@PathVariable String id, @Valid @RequestBody DocumentDTO documentDTO) {
         return ResponseEntity.ok(documentService.update(id, documentDTO));
     }
 
-    @Operation(tags = "DocumentQuery")
+    @Operation(
+            summary = "Get a document by ID",
+            description = "Returns the details of a specific document using its ID",
+            tags = {"DocumentQuery"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Document successfully retrieved",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = DocumentDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Document not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<DocumentDTO> getById(@PathVariable String id) {
         return ResponseEntity.ok(documentService.getById(id));
     }
 
-    @Operation(tags = "DocumentQuery")
+    @Operation(
+            summary = "Disable a document by ID",
+            description = "Disables a specific document using its ID",
+            tags = {"DocumentQuery"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Document successfully disabled",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = DocumentDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Document not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/disabled/{id}")
     public ResponseEntity<DocumentDTO> disabledById(@PathVariable String id) {
         return ResponseEntity.ok(documentService.disabledById(id));
