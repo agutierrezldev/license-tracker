8dd07b72c1252af051eb25c2a454b3ad17e54af2
axelgutierrezlopezdev <axelgutierrezlopezdev@gmail.com>
2024-10-06 15:28:17 -0500

    add documentation to customer-service and license-service  with swagger

--- customer-service/src/main/java/com/agutierrezl/customer_service/controller/CustomerController.java
+++ customer-service/src/main/java/com/agutierrezl/customer_service/controller/CustomerController.java
@@ -2,6 +2,16 @@ package com.agutierrezl.customer_service.controller;
 
 import com.agutierrezl.customer_service.dto.CustomerDTO;
 import com.agutierrezl.customer_service.service.ICustomerService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,40 +22,126 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/customers")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "customer-service",
+                version = "0.0.1-SNAPSHOT",
+                description = "Customer Service for project: license-tracker",
+                contact = @Contact(
+                        name = "Axel Gutierrez Lopez",
+                        email = "axelgutierrezlopezdev@gmail.com",
+                        url = "https://github.com/agutierrezldev"
+                ),
+                license = @License(
+                        url = "https://github.com/agutierrezldev",
+                        name = "Some License"
+                )
+        ),
+        servers = @Server(url = "http://localhost:9021"),
+        tags = {
+                @Tag(name = "CustomerQuery", description = "Customer management endpoints - read"),
+                @Tag(name = "CustomerCommand", description = "Customer management endpoints - write")
+        }
+)
 public class CustomerController {
 
     private final ICustomerService customerService;
 
+    @Operation(
+            summary = "Get all customers",
+            description = "Returns a list of all customers",
+            tags = {"CustomerQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of customers",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDTO.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("")
     public ResponseEntity<List<CustomerDTO>> getAll() {
         return ResponseEntity.ok(customerService.getAll());
     }
 
+    @Operation(
+            summary = "Create a new customer",
+            description = "Creates a new customer with the given details",
+            tags = {"CustomerCommand"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Customer successfully created",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request, invalid data"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @PostMapping("")
     public ResponseEntity<CustomerDTO> save(@Valid @RequestBody CustomerDTO customerDTO) {
         return ResponseEntity.ok(customerService.save(customerDTO));
     }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<DocumentDTO> update(@PathVariable String id, @Valid @RequestBody DocumentDTO documentDTO) {
-//        return ResponseEntity.ok(documentService.update(id, documentDTO));
-//    }
-//
+
+    @Operation(
+            summary = "Get customer by ID",
+            description = "Fetches a specific customer using their ID",
+            tags = {"CustomerQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Customer found",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Customer not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<CustomerDTO> getById(@PathVariable String id) {
         return ResponseEntity.ok(customerService.getById(id));
     }
 
+    @Operation(
+            summary = "Disable customer by ID",
+            description = "Disables a specific customer using their ID",
+            tags = {"CustomerQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Customer successfully disabled",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Customer not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("/disabled/{id}")
     public ResponseEntity<CustomerDTO> disabledById(@PathVariable String id) {
         return ResponseEntity.ok(customerService.disabledById(id));
     }
 
+    @Operation(
+            summary = "Enable customer by ID",
+            description = "Enables a specific customer using their ID",
+            tags = {"CustomerQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Customer successfully enabled",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Customer not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("/enabled/{id}")
     public ResponseEntity<CustomerDTO> enabledById(@PathVariable String id) {
         return ResponseEntity.ok(customerService.enabledById(id));
     }
 
+    @Operation(
+            summary = "Get all enabled customers",
+            description = "Returns a list of all enabled customers",
+            tags = {"CustomerQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of enabled customers",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDTO.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("/enabled")
     public ResponseEntity<List<CustomerDTO>> findAllEnabled() {
         return ResponseEntity.ok(customerService.findAllEnabled());
--- license-service/src/main/java/com/agutierrezl/license_service/controller/LicenseController.java
+++ license-service/src/main/java/com/agutierrezl/license_service/controller/LicenseController.java
@@ -2,6 +2,16 @@ package com.agutierrezl.license_service.controller;
 
 import com.agutierrezl.license_service.dto.LicenseDTO;
 import com.agutierrezl.license_service.service.ILicenseService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,42 +22,99 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/licenses")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "license-service",
+                version = "0.0.1-SNAPSHOT",
+                description = "License Service for project: license-tracker",
+                contact = @Contact(
+                        name = "Axel Gutierrez Lopez",
+                        email = "axelgutierrezlopezdev@gmail.com",
+                        url = "https://github.com/agutierrezldev"
+                ),
+                license = @License(
+                        url = "https://github.com/agutierrezldev",
+                        name = "Some License"
+                )
+        ),
+        servers = {
+                @Server(url = "http://localhost:9023", description = "Instance 1"),
+                // @Server(url = "http://localhost:9021", description = "Instance 2")
+        },
+        tags = {
+                @Tag(name = "LicenseQuery", description = "Endpoints para manipular datos en modo lectura"), //GET
+                @Tag(name = "LicenseCommand", description = "Endpoints para manipular datos en modo escritura") // POST, PUT, DELETE , PATCH
+        }
+)
 public class LicenseController {
 
     private final ILicenseService licenseService;
 
+    @Operation(
+            summary = "Get all licenses",
+            description = "Returns a list of all licenses",
+            tags = {"LicenseQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of licenses",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = LicenseDTO.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("")
     public ResponseEntity<List<LicenseDTO>> getAll() {
         return ResponseEntity.ok(licenseService.getAll());
     }
 
+    @Operation(
+            summary = "Create a new license",
+            description = "Creates a new license with the given details",
+            tags = {"LicenseCommand"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "License successfully created",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = LicenseDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request, invalid data"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @PostMapping("")
     public ResponseEntity<LicenseDTO> save(@Valid @RequestBody LicenseDTO licenseDTO) {
         return ResponseEntity.ok(licenseService.save(licenseDTO));
     }
 
+    @Operation(
+            summary = "Update license by ID",
+            description = "Updates the details of a specific license using its ID",
+            tags = {"LicenseCommand"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "License successfully updated",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = LicenseDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request, invalid data"),
+                    @ApiResponse(responseCode = "404", description = "License not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<LicenseDTO> update(@PathVariable String id, @Valid @RequestBody LicenseDTO licenseDTO) {
         return ResponseEntity.ok(licenseService.update(id, licenseDTO));
     }
 
+    @Operation(
+            summary = "Get license by ID",
+            description = "Fetches a specific license using its ID",
+            tags = {"LicenseQuery"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "License found",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = LicenseDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "License not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<LicenseDTO> getById(@PathVariable String id) {
         return ResponseEntity.ok(licenseService.getById(id));
     }
-//
-//    @GetMapping("/disabled/{id}")
-//    public ResponseEntity<CategoryDTO> disabledById(@PathVariable String id) {
-//        return ResponseEntity.ok(categoryService.disabledById(id));
-//    }
-//
-//    @GetMapping("/enabled/{id}")
-//    public ResponseEntity<CategoryDTO> enabledById(@PathVariable String id) {
-//        return ResponseEntity.ok(categoryService.enabledById(id));
-//    }
-//
-//    @GetMapping("/enabled")
-//    public ResponseEntity<List<CategoryDTO>> findAllEnabled() {
-//        return ResponseEntity.ok(categoryService.findAllEnabled());
-//    }
 }
